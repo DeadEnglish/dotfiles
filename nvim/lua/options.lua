728da3469e23f21c0d98696158059805f559cb97
Liam Canetti <git@liamcanetti.co.uk>
2023-12-24 17:03:19 +0000

    feat(plugins): add undotree and settings button to dashboard

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -9,6 +9,10 @@ vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 -- Disable swap files
 vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
 -- visual lines and numbering
 vim.opt.cursorline = true
 vim.opt.scrolloff = 8
