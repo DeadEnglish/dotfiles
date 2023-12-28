745171b782c7346aa67b28f6c75cddcd1477fe40
Liam Canetti <git@liamcanetti.co.uk>
2023-12-28 10:04:03 +0000

    chore(telescope): ignore node modules and git

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,9 +1,16 @@
+local telescopeConfig = {
+	defaults = {
+		file_ignore_patterns = { "node_modules", ".git" },
+	}
+}
+
 return {
 	{
 		"nvim-telescope/telescope.nvim",
 		tag = '0.1.5',
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
+			require("telescope").setup(telescopeConfig)
 			local builtin = require("telescope.builtin")
 			vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = "Find files"})
 			vim.keymap.set('n', '<leader>fg', builtin.live_grep, {desc = "Find in files"})
