ae9c8d23963e635cf764fcf601a8755a4053d70f
Liam Canetti <git@liamcanetti.co.uk>
2024-01-14 21:32:20 +0000

    feat(zsh): use p10k as theme

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -32,8 +32,8 @@ return {
 		-- Set menu
 		dashboard.section.buttons.val = {
 			dashboard.button("e", "		>	New file", ":ene <BAR><CR>"),
-			dashboard.button("f", "		>	Find file", ":Telescope find_files<CR>"),
-			dashboard.button("g", "		>	Grep files", ":Telescope live_grep<CR>"),
+			dashboard.button("f", "		>	Find file", ":Telescope find_files<CR>"),
+			dashboard.button("g", "󰺯		>	Grep files", ":Telescope live_grep<CR>"),
 			dashboard.button("r", "		>	Recent", ":Telescope oldfiles<CR>"),
 			dashboard.button("s", "		> Settings", ":e `=stdpath('config')`<CR>"),
 			dashboard.button("q", "󰛉		>	Quit NVIM", ":qa<CR>"),
