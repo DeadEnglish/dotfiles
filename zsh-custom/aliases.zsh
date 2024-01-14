1b913ddcb5358ad4185543c97e9822da66ae9fc5
Liam Canetti <liam.canetti@marshmallow.co>
2024-01-14 17:51:17 +0000

    chore: add more aliases

--- zsh-custom/aliases.zsh
+++ zsh-custom/aliases.zsh
@@ -1,5 +1,7 @@
 alias c="clear"
 alias gitrekt="git reset HEAD --hard"
+alias gitclean="git branch | grep -v "main" | xargs git branch -D"
 alias nuke="sudo rm -r ${1}"
 alias vim="nvim"
 alias gg="lazygit"
+alias pinentry='pinentry-mac'
