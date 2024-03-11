e2f648cf5ed77d10bff7627f02619a00c012bcd7
Liam Canetti <liam.canetti@marshmallow.co>
2024-03-11 06:41:11 +0000

    feat: notify for notifications and ufo for folding

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -19,3 +19,12 @@ vim.opt.scrolloff = 8
 vim.opt.number = true
 vim.opt.numberwidth = 2
 vim.opt.relativenumber = true
+
+-- Options for nvim ufo
+vim.o.foldcolumn = "1" -- '0' is not bad
+vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+
+-- Enable 24bit colours
+vim.opt.termguicolors = true
