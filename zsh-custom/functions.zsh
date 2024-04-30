7aaaf8bdd01c80b3c3e05cd8bb71126a689a34ef
Liam Canetti <liam.canetti@marshmallow.co>
2024-04-30 13:27:43 +0100

    feat(functions): add aws profile switcher

--- zsh-custom/functions.zsh
+++ zsh-custom/functions.zsh
@@ -4,3 +4,19 @@ killAtPort() {
   lsof -i TCP:$1 | grep LISTEN | awk '{print $2}' | xargs kill -9
   echo "Port" $1 "found and killed."
 }
+
+function setAwsProfile() {
+  export AWS_PROFILE="$1"
+  # Attempt to use the credentials to perform a lightweight AWS operation.
+  if aws sts get-caller-identity --profile "$AWS_PROFILE" > /dev/null 2>&1; then
+    echo "Already logged in as $AWS_PROFILE"
+  else
+    echo "Logging in as $AWS_PROFILE"
+    aws sso login --profile "$AWS_PROFILE"
+  fi
+  # After ensuring the login, export the necessary credentials and region.
+  eval "$(aws configure export-credentials --profile $AWS_PROFILE --format env)"
+  export AWS_REGION="eu-west-1"
+  echo "Now using $AWS_PROFILE"
+  echo "Credentials expire in $AWS_CREDENTIAL_EXPIRATION"
+}
