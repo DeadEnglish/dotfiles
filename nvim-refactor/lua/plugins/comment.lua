4b58e537e07365c942ed1d7bb202a4195c9ee1b4
Liam Canetti <git@liamcanetti.co.uk>
2024-07-20 22:05:22 +0100

    feat(plugins): comments

--- nvim-refactor/lua/plugins/comment.lua
+++ nvim-refactor/lua/plugins/comment.lua
@@ -1,7 +1,19 @@
 return {
-	'numToStr/Comment.nvim',
+	"numToStr/Comment.nvim",
+	event = { "BufEnter" },
+	dependencies = {
+		"JoosepAlviste/nvim-ts-context-commentstring",
+		config = function()
+			require("nvim-ts-context-commentstring").setup({ enable_autocmd = false })
+		end,
+	},
 	lazy = false,
 	config = function()
-		require('Comment').setup()
+		require("Comment").setup({
+			pre_hook = require("ts_context_commentstrings.integrations.comment_nvim").create_pre_hook(),
+		})
+
+		local ft = require("Comment.ft")
+		ft.set("reason", { "//%s", "/*%s*/" })
 	end,
 }
