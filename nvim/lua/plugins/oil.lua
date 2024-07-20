eab2002e8c5bf0779638f3937108b4db10975f24
Liam Canetti <git@liamcanetti.co.uk>
2024-07-21 00:36:25 +0100

    fix: floating oil

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,9 +1,9 @@
 return {
 	"stevearc/oil.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
 	opts = {},
+	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		require("oil").setup({ view_options = { show_hidden = true } })
-		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+		vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
 	end,
 }
