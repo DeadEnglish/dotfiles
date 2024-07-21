dff7867c23d420bd24de0ab35da5992e24798264
Liam Canetti <git@liamcanetti.co.uk>
2024-07-21 15:11:54 +0100

    fix(treesitter): correct branch version rather than tag

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -14,7 +14,7 @@ local telescopeConfig = {
 return {
 	{
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.x",
+		branch = "0.1.x",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			require("telescope").setup(telescopeConfig)
