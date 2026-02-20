e4b782b31eda6b5fc27837b1c76a73dbfd76d926
Liam Canetti <git@liamcanetti.co.uk>
2026-02-20 12:25:23 +0000

    chore: upgrade telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -14,7 +14,7 @@ local telescopeConfig = {
 return {
 	{
 		"nvim-telescope/telescope.nvim",
-		branch = "0.1.x",
+		version = "v0.2.1",
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			require("telescope").setup(telescopeConfig)
